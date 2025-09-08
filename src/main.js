0b5d8cd24eb94cc05b7db57d00d518ff7bb9a2f0
winoooops <winoooops@outlook.com>
2025-09-08 17:17:47 +0800

    feat: add barebones to the tax-calculator

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import './style.css';
+
+// add htmx to window object for global use
+import htmx from 'htmx.org';
+window.htmx = htmx;
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import inject from '@rollup/plugin-inject';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    inject({
+      htmx: 'htmx.org', // Injects htmx from the 'htmx.org' package
+    }),
+  ],
+});
